1adfadf692d19914e267c9087e639880f5cfa239
AlanLFM <113216725+AlanLFM@users.noreply.github.com>
2024-08-19 20:22:37 -0600

    Agrego Lista de contratos con backend

---
+++ src/components/ListaContratos/ListaContratos.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ListaContratos = () => {
+    const [contracts, setContracts] = useState([]);
+
+    useEffect(() => {
+        const fetchContracts = async () => {
+            try {
+                const response = await axios.get(`${process.env.URL-API}/api/contracts`); // Hace la solicitud a la API para obtener los contratos
+                setContracts(response.data); // Guarda los contratos en el estado
+            } catch (error) {
+                console.error('Error al obtener la lista de contratos:', error);
+            }
+        };
+
+        fetchContracts();
+    }, []);
+
+    return (
+        <div>
+            <h1>Organizaciones</h1>
+            <ul>
+                {contracts.map(contract => (
+                    <li key={contract._id}>
+                        <h2>{contract.name}</h2>
+                        <p>Dirección del contrato: {contract.contractAddress}</p>
+                        <p>{contract.description}</p>
+                        <p>Propietario: {contract.owner}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ListaContratos;
