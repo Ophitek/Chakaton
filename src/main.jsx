543a35570687e6a5d6fc7b243feab59e76403235
KiranFaux <delgado.lucero.cristian@gmail.com>
2024-08-19 21:32:13 -0600

    El front mas decente del momento

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import "./css/main.css"
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Buffer } from 'buffer';
+
 window.Buffer = Buffer; 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
